f45591129770e32f16eccea7a48d6572f346d14e
f3ci <48576458+f3ci@users.noreply.github.com>
2021-02-05 10:23:33 +0700

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+prog=/usr/bin/vncpasswd
+#default paassword
+mypass="123456"
+
+/usr/bin/expect <<EOF
+spawn "$prog"
+expect "Password:"
+send "$mypass\r"
+expect "Verify:"
+send "$mypass\r"
+expect "Would you like to enter a view-only password (y/n)?"
+send "y\r"
+expect "Password:"
+send "$mypass\r"
+expect "Verify:"
+send "$mypass\r"
+expect eof
+exit
+EOF
+
+export USER=root
+vncserver :0 -geometry 1920x1080 -depth 16
+vncserver -kill :0
+echo "/usr/bin/startx" >> /root/.vnc/xstartup
+vncserver :0 -geometry 1920x1080 -depth 16
+
+/usr/share/novnc/utils/launch.sh --listen 8080 --vnc localhost:5900
