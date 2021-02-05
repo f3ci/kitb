3018e35fa1ef7fccade136b3ae37fa0f917ddc6d
f3ci <48576458+f3ci@users.noreply.github.com>
2021-02-05 15:32:05 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -21,6 +21,12 @@ exit
 EOF
 
 export USER=root
+
+FILE=/tmp/.X11-unix/X0
+if [ -f "$FILE" ]; then
+    rm $FILE /tmp/.X0-lock
+fi
+
 vncserver :0 -geometry 1920x1080 -depth 16
 vncserver -kill :0
 echo "/usr/bin/startx" >> /root/.vnc/xstartup
